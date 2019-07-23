3d7c27966e73a98444222ab1a4932142e25bd63a
David Dymko <dymkod@gmail.com>
2019-07-23 08:35:33 -0400

    2.0.1 version mismatch

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Change Log
 
+## v2.0.1 (2019-07-23)
+### Fixes
+* Version Mismatch issue
+
+
 ## v2.0.0 (2019-06-25)
 ### Features
 * Various bug fixes
--- addons/vultr/Configuration.php
+++ addons/vultr/Configuration.php
@@ -51,7 +51,7 @@ class Configuration extends main\mgLibs\process\AbstractConfiguration
 	 * Module version
 	 * @var string
 	 */
-	public $version = '2.0.0';
+	public $version = '2.0.1';
 
 	/**
 	 * Module author
